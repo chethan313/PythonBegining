43dc002d2be08c2e34bdb7d8eae1604373de7e71
Chethan P S <165404902+chethan313@users.noreply.github.com>
2025-02-16 19:58:30 +0530

    Add files via upload

---
+++ Program2.py
@@ -0,0 +1,11 @@
+#Program to find Square root 
+num=64
+sr=num**(0.5) #also can use 1/2 instead of 0.5
+print("The square root of the given numbers is :",sr)
+
+
+#using math module
+import math
+num=int(input("Enter a number:"))
+sr=math.sqrt(num)
+print("The square root of the entered number is :",sr)
