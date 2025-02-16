6cfa83324dc03f23d93370ed457bea0249f2ba7b
Chethan P S <165404902+chethan313@users.noreply.github.com>
2025-02-16 19:51:13 +0530

    Update Program1.py

---
+++ Program1.py
@@ -0,0 +1,7 @@
+#Calculate_BMI
+weight=int(input("Enter your weight in kg:"))
+height=float(input("Enter your height in meters:"))
+
+BMI=weight/(height*height)
+
+print("The body mass index is equal to:",BMI)
